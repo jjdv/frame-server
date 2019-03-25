8471f6230422a409304df810950e997d10e74d1b
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-03-25 23:40:41 +0100

    changed to use 'process.exit()' if errors

--- bin/frame-server.cli.js
+++ bin/frame-server.cli.js
@@ -2,39 +2,40 @@
 'use strict'
 
 // find local server config and merge with ini config data
+// if errors are found, they are reported and null is returned
 let serverConfig = require('../config/server.config')
+if (!serverConfig) process.exit(0)
 
 // validate the config data
 // if errors are found, they are reported and null is returned
 const validateConfigData = require('../modules/validation/validate-config-data')
 serverConfig = validateConfigData(serverConfig)
+if (!serverConfig) process.exit(0)
 
-if (serverConfig) {
-  const app = require('express')()
+const app = require('express')()
 
-  // set views if defined
-  const setView = require('../modules/deployment/set-view')
-  setView(app, serverConfig.view)
+// set views if defined
+const setView = require('../modules/deployment/set-view')
+setView(app, serverConfig.view)
 
-  const { middlewares } = require('../modules/middlewares/middlewares')
-  const { serverMiddlewares, siteMiddlewares, serveDynamicFiles, serveStaticFiles, wrongRequestHandler } = middlewares(serverConfig)
+const { middlewares } = require('../modules/middlewares/middlewares')
+const { serverMiddlewares, siteMiddlewares, serveDynamicFiles, serveStaticFiles, wrongRequestHandler } = middlewares(serverConfig)
 
-  // deploy server middlewares
-  serverMiddlewares.apply(app)
+// deploy server middlewares
+serverMiddlewares.apply(app)
 
-  // deploy site middlewares defined for the site
-  siteMiddlewares.apply(app)
+// deploy site middlewares defined for the site
+siteMiddlewares.apply(app)
 
-  // serve special files for defined route paths
-  serveDynamicFiles.apply(app)
+// serve special files for defined route paths
+serveDynamicFiles.apply(app)
 
-  // serve static files from defined directories for defined route paths
-  serveStaticFiles.apply(app)
+// serve static files from defined directories for defined route paths
+serveStaticFiles.apply(app)
 
-  // handle pure static files, limited to specified extensions, if defined
-  wrongRequestHandler.apply(app)
+// handle pure static files, limited to specified extensions, if defined
+wrongRequestHandler.apply(app)
 
-  // start the server
-  const startServer = require('../modules/deployment/start-server')
-  startServer(app, serverConfig.port)
-}
+// start the server
+const startServer = require('../modules/deployment/start-server')
+startServer(app, serverConfig.port)
