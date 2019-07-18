fb76e68f9e7f8ce746956f83dc54b27e2a604837
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-07-18 23:08:10 +0200

    complete tests for first 3 functions

--- test/modules/helpers/basic.test.onhold.js
+++ test/modules/helpers/basic.test.onhold.js
@@ -2,32 +2,135 @@
 'use strict'
 
 // test environment
+const path = require('path')
 const { expect, sinon } = require('../../test-env')
 
 // method under tets
-const { filePath } = require('../../../modules/helpers/basic')
+const {
+  filePath,
+  filePathNotEmpty,
+  routePathsErr,
+  nameErr,
+  isEmpty
+} = require('../../../modules/helpers/basic')
 
 // test data
-const { falsy, validDirName, invalidDirName, validFileName, invalidFileName } = require('./basic.data')
+const {
+  falsy,
+  nonStringFilePaths,
+  validDirName,
+  invalidDirName,
+  validFileName,
+  invalidFileName,
+  testPathsData
+} = require('./test-support/basic.data')
+
+// test variables
+const reportErr = sinon.spy()
+class StatusStub {
+  constructor() {
+    this.error = false
+    this.reportErr = function(...errMsgs) {
+      this.error = true
+      reportErr(...errMsgs)
+    }
+  }
+}
+const status = new StatusStub()
 let res
 
-// test body
-describe('modules > helpers > basic.js', function () {
-  describe('#filePath()', function () {
-    describe('should return null for', function () {
-      it('falsy paths', () => {
-        falsy.forEach(falsyVal => {
-          res = filePath(falsyVal)
-          expect(res).to.be.null()
-        })
+describe('modules > helpers > basic.js', function() {
+  beforeEach(function() {
+    status.error = false
+    reportErr.resetHistory()
+  })
+
+  describe('filePath()', function() {
+    it('returns null for falsy paths but does not report an error', function() {
+      falsy.forEach(falsyVal => {
+        res = filePath(falsyVal)
+        expect(res).to.be.null()
+        reportErr.should.have.not.been.called()
       })
-      it('invalid paths', function () {
-        const status = { reportErr: sinon.spy() }
-        res = filePath(invalidFileName, validDirName, 'testName', status)
+    })
+
+    filePathBaseTests(filePath)
+  })
+
+  describe('filePathNotEmpty()', function() {
+    it('returns null and reports error for empty/falsy path', function() {
+      falsy.forEach(falsyVal => {
+        res = filePathNotEmpty(falsyVal, null, 'testName', status)
         expect(res).to.be.null()
-        res = filePath(validFileName, invalidDirName, 'testName', status)
-        expect(res).to.be.null()
+        reportErr.lastCall.should.have.been.calledWith(
+          "Error: The specification of the file path in the 'testName' cannot be an empty string."
+        )
+      })
+    })
+
+    filePathBaseTests(filePathNotEmpty)
+  })
+
+  describe('routePathsErr()', function() {
+    it('returns false for valid paths', function() {
+      testPathsData.valid.forEach(td => {
+        const res = routePathsErr(td.paths, td.varName, status)
+        expect(res).to.be.false()
+        reportErr.should.have.not.been.called()
+      })
+    })
+
+    it('returns true and reports relevant error for invalid paths', function() {
+      testPathsData.invalid.forEach(td => {
+        reportErr.resetHistory()
+        const res = routePathsErr(td.paths, td.varName, status)
+        expect(res).to.be.true()
+        expect(reportErr.callCount).to.equal(td.errMsg.length)
+        td.errMsg.forEach((errMsg, indx) => {
+          reportErr.getCall(indx).calledWithExactly(errMsg)
+        })
       })
     })
   })
 })
+
+// -------------------------------------------------------
+// helpers
+// -------------------------------------------------------
+
+function filePathBaseTests(filePathTestFn) {
+  let resolvedFilePath
+
+  it('returns null and reports error for non-string path', function() {
+    nonStringFilePaths.forEach(nonStrVal => {
+      res = filePathTestFn(nonStrVal, null, 'testName', status)
+      expect(res).to.be.null()
+      reportErr.lastCall.should.have.been.calledWith(
+        "Error: Wrong file path format of 'testName':",
+        nonStrVal
+      )
+    })
+  })
+
+  it('returns null and reports error for invalid paths', function() {
+    res = filePathTestFn(invalidFileName, validDirName, 'testName', status)
+    expect(res).to.be.null()
+    resolvedFilePath = path.resolve(validDirName, invalidFileName)
+    reportErr.lastCall.should.have.been.calledWith(
+      `Error: Cannot find "${resolvedFilePath}" specified by the 'testName' as "${invalidFileName}"`
+    )
+
+    res = filePathTestFn(validFileName, invalidDirName, 'testName', status)
+    expect(res).to.be.null()
+    resolvedFilePath = path.resolve(invalidDirName, validFileName)
+    reportErr.lastCall.should.have.been.calledWith(
+      `Error: Cannot find "${resolvedFilePath}" specified by the 'testName' as "${validFileName}"`
+    )
+  })
+
+  it('returns filePath for valid paths', function() {
+    res = filePathTestFn(validFileName, validDirName, 'testName', status)
+    resolvedFilePath = path.resolve(validDirName, validFileName)
+    expect(res).to.be.equal(resolvedFilePath)
+  })
+}
--- test/modules/helpers/test-support/basic.data.js
+++ test/modules/helpers/test-support/basic.data.js
@@ -1,9 +1,53 @@
-'use strict'
-
 const path = require('path')
 
-exports.falsy = [ false, null, undefined, '' ]
+/**
+ * test data for filePath and filePathNotEmpty
+ */
+
+exports.falsy = [false, null, undefined, '']
+exports.nonStringFilePaths = [{}, []]
 exports.validDirName = __dirname
-exports.invalidDirName = path.join(exports.validDirName, 'aaa')
+exports.invalidDirName = path.join(__dirname, 'aaa')
 exports.validFileName = path.basename(__filename)
-exports.invalidFileName = path.join(exports.validFileName, 'aaa')
+exports.invalidFileName = path.join(__filename, 'aaa')
+
+/**
+ * test data for routePathsErr
+ */
+
+exports.testPathsData = {
+  valid: [
+    {
+      paths: '/',
+      result: false
+    },
+    {
+      paths: /abc/,
+      result: false
+    },
+    {
+      paths: ['/', '/some/path', /aaa/],
+      result: false
+    }
+  ],
+  invalid: [
+    {
+      paths: '',
+      varName: 'testVarName',
+      result: true,
+      errMsg: [
+        "Error: The specification of the path in 'testVarName' cannot be an empty string."
+      ]
+    },
+    {
+      paths: ['/', '', 'some/path', '/good', undefined],
+      varName: 'testVarName',
+      result: true,
+      errMsg: [
+        "Error: The specification of the path in 'testVarName' cannot be an empty string.",
+        "Error: Route paths in 'testVarName' should be absolute, i.e. start with: '/' but is defined as: 'some/path'.",
+        "Wrong path format in 'testVarName'."
+      ]
+    }
+  ]
+}
