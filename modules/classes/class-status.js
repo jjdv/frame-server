392ea453c21f5c348df89818a5240d29e6b2ce31
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-07-18 23:01:54 +0200

    change to simple class definition

--- modules/classes/class-status.js
+++ modules/classes/class-status.js
@@ -2,54 +2,38 @@
 
 const { toReadOnlyProps } = require('../helpers/object')
 
-module.exports = function Status (props) {
-  const intV = { error: false }
-  Object.defineProperties(this, {
-    error: {
-      get: () => intV.error
-    },
-    reset: {
-      value: () => { intV.error = false }
-    }
-  })
-
-  if (!props) {
-    Object.defineProperty(this, 'reportErr', {
-      value: (...errMsgs) => reportErr(intV, ...errMsgs)
-    })
-  } else {
-    if (Array.isArray(props)) {
+module.exports = class Status {
+  constructor(props) {
+    this.error = false
+    if (!props) {
+      this.reportErr = (...errMsgs) => {
+        this.error
+        if (errMsgs.length) console.error('Error: ', ...errMsgs)
+      }
+    } else if (Array.isArray(props)) {
       for (let prop of props) {
         if (!prop || typeof prop !== 'string') {
-          console.error('Error: Invalid property name for creating Status instance: ', prop, '\nExpected non-empty string ... Property skipped.')
+          console.error(
+            'Error: Invalid property name for creating Status instance: ',
+            prop,
+            '\nExpected non-empty string ... Property skipped.'
+          )
           continue
         }
-        this[prop] = {}
-        intV[prop] = { error: false }
-        Object.defineProperties(this[prop], {
-          reportErr: {
-            value: (...errMsgs) => reportErr([intV, intV[prop]], ...errMsgs)
-          },
-          error: {
-            get: () => intV[prop].error
-          },
-          reset: {
-            value: () => { intV[prop].error = false }
+        this[prop] = {
+          error: false,
+          reportErr: (...errMsgs) => {
+            this.error = true
+            this[prop].error = true
+            if (errMsgs.length) console.error('Error: ', ...errMsgs)
           }
-        })
+        }
       }
-      toReadOnlyProps(this)
-    } else console.error('Error: Invalid arguments for creating Status instance: ', props, '\nExpected array ...')
+    } else
+      console.error(
+        'Error: Invalid arguments for creating Status instance: ',
+        props,
+        '\nExpected array ...'
+      )
   }
-}
-
-//
-// -------------------------------------------------------------------------------
-// supporting functions
-// -------------------------------------------------------------------------------
-
-function reportErr (status, ...errMsgs) {
-  if (errMsgs.length) console.error('Error: ', ...errMsgs)
-  if (Array.isArray(status)) status.forEach(statusEl => { statusEl.error = true })
-  else status.error = true
 }
