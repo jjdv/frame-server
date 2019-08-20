97cc9f46242d5db5af7b5a4945e1763044a09107
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-08-20 23:00:34 +0200

    initial code structure cleanup

--- modules/classes/middlewares.js
+++ modules/classes/middlewares.js
@@ -1,21 +1,18 @@
-const { nameErr } = require('../helpers/basic')
-const { createGetOnlyProps } = require('../helpers/object')
+const { nameErr, isEmpty } = require('../helpers/basic')
 const Middleware = require('./middleware')
 const Status = require('./status')
 
-// function Middlewares(middlewaresName, middlewares, applyMsg) {
-//   if (!Array.isArray(middlewares)) middlewares = [middlewares]
 //   if (middlewaresArgsErr(middlewaresName, middlewares, applyMsg)) return
-
-//   createGetOnlyProps(this, { name: middlewaresName, middlewares, applyMsg })
-// }
 
 class Middlewares {
   constructor(middlewaresName, middlewaresDef, options, applyMsg) {
     const status = new Status()
 
-    if (nameErr(middlewaresName, 'middlewares group', middlewares, status))
-      return null
+    if (
+      nameErr(middlewaresName, 'middlewares group', middlewares, status) ||
+      isEmpty(middlewaresDef)
+    )
+      return
 
     if (!Array.isArray(middlewaresDef)) middlewaresDef = [middlewaresDef]
     const middlewares = []
@@ -35,58 +32,18 @@ class Middlewares {
   }
 
   apply(app, groupReporting = true) {
-    if (!app || !this.middlewares) return
+    if (!app || isEmpty(this.middlewares)) return
 
     const individualReporting = !groupReporting
-    if (Array.isArray(this.middlewares)) {
-      if (groupReporting) {
-        const mNames = this.middlewares.map(m => m.name)
-        const applyMsg = this.applyMsg
-          ? this.applyMsg
-          : `The middlewares applied from the '${this.name}': `
-        console.log(applyMsg, mNames)
-      }
-      this.middlewares.forEach(m => m.apply(app, individualReporting))
-    } else this.middlewares.apply(app)
+    if (groupReporting) {
+      const mNames = this.middlewares.map(m => m.name)
+      const applyMsg = this.applyMsg
+        ? this.applyMsg
+        : `The middlewares applied from the '${this.name}': `
+      console.log(applyMsg, mNames)
+    }
+    this.middlewares.forEach(m => m.apply(app, individualReporting))
   }
 }
 
 module.exports = Middlewares
-
-// -----------------------------------------------------------------------
-// supporting functions
-// -----------------------------------------------------------------------
-
-function middlewaresArgsErr(middlewaresName, middlewares, applyMsg) {
-  const status = new Status()
-  nameErr(middlewaresName, 'middlewares group', middlewares, status)
-  middlewaresErrCheck(middlewares, middlewaresName, status)
-  if (applyMsg && typeof applyMsg !== 'string') {
-    status.reportErr(
-      `Format of 'applyMsg' in '${middlewaresName}' should be string and not: `,
-      applyMsg
-    )
-  }
-
-  return status.error
-}
-
-function middlewaresErrCheck(middlewares, middlewaresName, status) {
-  if (Array.isArray(middlewares))
-    middlewares.forEach(m => middlewareErrCheck(m, middlewaresName, status))
-  else middlewareErrCheck(middlewares, middlewaresName, status)
-}
-
-function middlewareErrCheck(middlewareObj, middlewaresName, status) {
-  if (middlewareObj.constructor !== Middleware) {
-    const extraInfo =
-      middlewaresName && typeof middlewaresName === 'string'
-        ? ''
-        : ` in the middleware group: ${middlewaresName}.`
-    status.reportErr(
-      'Wrong format of the middleware: ',
-      middlewareObj,
-      extraInfo
-    )
-  }
-}
--- modules/helpers/middlewares.js
+++ modules/helpers/middlewares.js
@@ -1,8 +1,53 @@
-validateDefs = function(defs, validateFn, status) {
+const { nameErr } = require('./basic')
+
+function validateDefs(defs, validateFn, status) {
   if (!defs) return
 
   if (!Array.isArray(defs)) defs = [defs]
   defs.forEach((def, index) => validateFn(def, index, status))
 }
 
-module.exports = { validateDefs }
+function middlewaresArgsErr(middlewaresName, middlewares, applyMsg, status) {
+  if (!status) status = new Status()
+
+  nameErr(middlewaresName, 'middlewares group', middlewares, status)
+  middlewaresErrCheck(middlewares, middlewaresName, status)
+  applyMsgErrCheck(applyMsg, middlewaresName, status)
+
+  return status.error
+}
+
+module.exports = { validateDefs, middlewaresArgsErr }
+
+// -----------------------------------------------------------------------
+// helpers
+// -----------------------------------------------------------------------
+
+function middlewaresErrCheck(middlewares, middlewaresName, status) {
+  if (Array.isArray(middlewares))
+    middlewares.forEach(m => middlewareErrCheck(m, middlewaresName, status))
+  else middlewareErrCheck(middlewares, middlewaresName, status)
+}
+
+function middlewareErrCheck(middlewareObj, middlewaresName, status) {
+  if (middlewareObj.constructor !== Middleware) {
+    const extraInfo =
+      middlewaresName && typeof middlewaresName === 'string'
+        ? ''
+        : ` in the middleware group: ${middlewaresName}.`
+    status.reportErr(
+      'Wrong format of the middleware: ',
+      middlewareObj,
+      extraInfo
+    )
+  }
+}
+
+function applyMsgErrCheck(applyMsg, middlewaresName, status) {
+  if (applyMsg && typeof applyMsg !== 'string') {
+    status.reportErr(
+      `Format of 'applyMsg' in '${middlewaresName}' should be string and not: `,
+      applyMsg
+    )
+  }
+}
