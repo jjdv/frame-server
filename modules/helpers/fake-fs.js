e4f6c38cbd9deab7396d82f8fbf77344337627de
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-09-10 22:45:09 +0200

    changed to FakeFs class

--- modules/helpers/fake-fs.js
+++ modules/helpers/fake-fs.js
@@ -1,36 +1,51 @@
-const fakeFilePaths = []
-const fakeDirPaths = []
+module.exports = function FakeFs () {
+  const fakeFilePaths = []
+  const fakeDirPaths = []
 
-function resetFakePaths () {
-  fakeFilePaths.length = 0
-  fakeDirPaths.length = 0
+  function addFakeFilePaths (filePaths) {
+    if (!filePaths) return
+
+    if (filePaths.constructor === Array)
+      filePaths.forEach(fp => fakeFilePaths.push(fp))
+    else fakeFilePaths.push(filePaths)
+  }
+
+  function addFakeDirPaths (dirPaths) {
+    if (!dirPaths) return
+
+    if (dirPaths.constructor === Array)
+      dirPaths.forEach(dp => fakeDirPaths.push(dp))
+    else fakeDirPaths.push(dirPaths)
+  }
+
+  function resetFakeFilePaths () {
+    fakeFilePaths.length = 0
+  }
+
+  function resetFakeDirPaths () {
+    fakeDirPaths.length = 0
+  }
+
+  function resetFakePaths () {
+    resetFakeFilePaths()
+    resetFakeDirPaths()
+  }
+
+  function fakeFsExistsSync (path) {
+    return fakeFilePaths.includes(path) || fakeDirPaths.includes(path)
+  }
+
+  function fakeFsStatSync (path) {
+    if (fakeFilePaths.includes(path)) return { isFile: () => true }
+    else if (fakeDirPaths.includes(path)) return { isDirectory: () => true }
+    return {}
+  }
+
+  this.addFakeFilePaths = addFakeFilePaths
+  this.addFakeDirPaths = addFakeDirPaths
+  this.resetFakeFilePaths = resetFakeFilePaths
+  this.resetFakeDirPaths = resetFakeDirPaths
+  this.resetFakePaths = resetFakePaths
+  this.existsSync = fakeFsExistsSync
+  this.statSync = fakeFsStatSync
 }
-
-function addFakeFilePaths (filePaths) {
-  if (filePaths.constructor === Array)
-    filePaths.forEach(fp => fakeFilePaths.push(fp))
-  else fakeFilePaths.push(filePaths)
-}
-
-function addFakeDirPaths (dirPaths) {
-  if (dirPaths.constructor === Array)
-    dirPaths.forEach(dp => fakeDirPaths.push(dp))
-  else fakeDirPaths.push(dirPaths)
-}
-
-function fakeFsExistsSync (path) {
-  return fakeFilePaths.includes(path) || fakeDirPaths.includes(path)
-}
-
-function fakeFsStatSync (path) {
-  if (fakeFilePaths.includes(path)) return { isFile: () => true }
-  else if (fakeDirPaths.includes(path)) return { isDirectory: () => true }
-  return {}
-}
-
-const fakeFs = {
-  existsSync: fakeFsExistsSync,
-  statSync: fakeFsStatSync
-}
-
-module.exports = { fakeFs, resetFakePaths, addFakeDirPaths, addFakeFilePaths }
