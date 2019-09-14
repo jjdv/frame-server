c76014f965f4a37e3b563ff878b89e703e39ac65
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-09-14 20:48:20 +0200

    file name change

--- test/modules/helpers/error-reporters.test.js
+++ test/modules/helpers/error-reporters.test.js
@@ -3,16 +3,16 @@
 
 // test environment
 const path = require('path')
-const { expect, sinon } = require('../../test-env')
+const { expect, sinon } = require('../../test-support/test-env')
 
 const Status = require('../../../modules/classes/status')
 
 // methods under test
 const {
   validatedDirectory,
-  validateView,
   filePath,
   filePathRequired,
+  safeRequire,
   routePathsErr,
   nameErr
 } = require('../../../modules/helpers/error-reporters')
@@ -21,16 +21,13 @@ const {
 const {
   falsyVars,
   stringValues,
-  nonObjectValues,
   nonStringValues,
   testPathsData,
   validDirPath,
-  validFilePath,
   validFileNameStr,
   invalidDirPath,
   invalidFilePath
 } = require('./test-support/basic.test.data')
-const validViews = require('./test-support/error-reporters.test.data')
 
 // test variables
 let status, consoleErrorStub, res, varName, varDef
@@ -54,16 +51,16 @@ describe('modules > helpers > basic.js', () => {
     testValidatedDirectory()
   })
 
-  describe('validateView()', () => {
-    testValidateView()
-  })
-
   describe('filePath()', () => {
     testFilePath()
   })
 
   describe('filePathRequired()', () => {
     testFilePathRequired()
+  })
+
+  describe('safeRequire()', () => {
+    testSafeRequire()
   })
 
   describe('routePathsErr()', () => {
@@ -121,75 +118,6 @@ function testValidatedDirectory () {
   })
 }
 
-function testValidateView () {
-  it('reports error and returns status.error true for non-object values', () => {
-    nonObjectValues.forEach(nonObjectVal => {
-      consoleErrorStub.resetHistory()
-      validateView(nonObjectVal, status)
-      expect(status.error).to.be.true()
-      consoleErrorStub.should.have.been.calledOnceWithExactly(
-        'Error: ',
-        'Wrong format of the view definition in the server config file:',
-        nonObjectVal
-      )
-    })
-  })
-
-  it('reports error and returns status.error true for non-string, not falsy view.engine values', () => {
-    nonStringValues.forEach(nonObjectVal => {
-      consoleErrorStub.resetHistory()
-      validateView({ engine: nonObjectVal }, status)
-      expect(status.error).to.be.true()
-      consoleErrorStub.should.have.been.calledOnceWithExactly(
-        'Error: ',
-        'view.engine in the server config file must be a string, not: ',
-        nonObjectVal
-      )
-    })
-  })
-
-  it('reports error and returns status.error true for invalid, not falsy view.dir values', () => {
-    ;[{}, /abc/, 44].forEach(wrongFormatVal => {
-      consoleErrorStub.resetHistory()
-      validateView({ dir: wrongFormatVal }, status)
-      expect(status.error).to.be.true()
-      consoleErrorStub.should.have.been.calledOnceWithExactly(
-        'Error: ',
-        'view.dir in the server config file must be a string or an array of strings, not: ',
-        wrongFormatVal
-      )
-    })
-
-    process.env.APP_ROOT_DIR = 'c:/root'
-    const testDirArrs = [
-      'abc',
-      ['...', invalidDirPath],
-      [invalidFilePath, validFilePath]
-    ]
-    testDirArrs.forEach(wrongFormatVal => {
-      consoleErrorStub.resetHistory()
-      validateView({ dir: wrongFormatVal }, status)
-      expect(status.error).to.be.true()
-      if (!Array.isArray(wrongFormatVal)) wrongFormatVal = [wrongFormatVal]
-      wrongFormatVal.forEach((wfVal, index) => {
-        const dir = path.resolve('c:/root', wfVal)
-        consoleErrorStub
-          .getCall(index)
-          .should.have.been.calledWithExactly(
-            'Error: ',
-            `Cannot find directory "${dir}" specified in 'view.dir'.`
-          )
-      })
-    })
-  })
-
-  it('returns status.error false for valid view definitions', () => {
-    validViews.forEach(validView => validateView(validView, status))
-    expect(status.error).to.be.false()
-    consoleErrorStub.should.have.not.been.called()
-  })
-}
-
 function testFilePath () {
   it('returns null for falsy paths but does not report an error', () => {
     falsyVars.forEach(falsyVal => {
@@ -214,8 +142,22 @@ function testFilePathRequired () {
       "The specification of the file path in the 'testName' cannot be empty."
     )
   })
+}
 
-  filePathBaseTests(filePathRequired)
+function testSafeRequire () {
+  const rootDir = path.resolve(__dirname, '../..')
+  const validFilePath = 'test-support/test-function.js'
+  let testFn
+
+  it('provides the required data if the required file exists', () => {
+    testFn = safeRequire(validFilePath, rootDir, 'test-function')
+    expect(testFn.toString()).to.equal((() => 'test').toString())
+  })
+
+  it('provides null if the required file does not exist', () => {
+    testFn = safeRequire(validFilePath + 'abc', rootDir, 'test-function')
+    expect(testFn).to.be.null
+  })
 }
 
 function testRoutePathsErr () {
