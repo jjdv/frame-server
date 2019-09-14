92f4f21ae5b9ef5d05dea1dcd31408cb623cf655
Jacek Jakubik <36626288+jjdv@users.noreply.github.com>
2019-09-14 20:55:30 +0200

    validation moved to external function

--- modules/classes/middleware.js
+++ modules/classes/middleware.js
@@ -1,21 +1,16 @@
 const Status = require('./status')
 const { isEmpty } = require('../helpers/basic')
-const { routePathsErr, nameErr } = require('../helpers/error-reporters')
-const {
-  middlewareMock,
-  middlewareDefToArgs,
-  middlewareFnErrCheck,
-  middlewareTypeErrCheck
-} = require('../helpers/middleware')
+const { middlewareMock, middlewareDefToArgs } = require('./helpers/middleware')
 
 class Middleware {
-  constructor(mDef, options = {}) {
-    const status = new Status()
-
-    if (!mDef || mDef.constructor !== Object || isEmpty(mDef)) {
-      status.reportErr('Invalid middleware definition: ', mDef)
-      for (const prop in middlewareMock) this[prop] = middlewareMock[prop]
-      return
+  constructor (middlewareDef, options = {}, status = new Status()) {
+    if (
+      !middlewareDef ||
+      middlewareDef.constructor !== Object ||
+      isEmpty(middlewareDef)
+    ) {
+      status.reportErr('Invalid middleware definition: ', middlewareDef)
+      return middlewareMock
     }
 
     const [
@@ -23,12 +18,7 @@ class Middleware {
       middlewareFn,
       routePaths,
       type
-    ] = middlewareDefToArgs(mDef, options)
-
-    nameErr(middlewareName, 'middleware', mDef, status)
-    middlewareFnErrCheck(middlewareFn, middlewareName, status)
-    if (routePaths) routePathsErr(routePaths, middlewareName, status)
-    middlewareTypeErrCheck(type, middlewareName, status)
+    ] = middlewareDefToArgs(middlewareDef, options, status)
 
     this.name = middlewareName
     if (status.error) this.middlewareFn = null
@@ -40,7 +30,8 @@ class Middleware {
   }
 
   apply (app, report = true) {
-    if (!app || !this.middlewareFn) return
+    if (!(app instanceof Function) || !(this.middlewareFn instanceof Function))
+      return
 
     if (this.routePaths) app[this.type](this.routePaths, this.middlewareFn)
     else app[this.type](this.middlewareFn)
